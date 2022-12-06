cb576c1e42dc2807af7d4e4e273ab3fbc64bca21
thyagosic <thyagosic@gmail.com>
2022-12-06 00:08:37 -0300

    Initial commit

---
+++ src/Api.CrossCutting/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace crosscutting;
+public class Class1
+{
+
+}
---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=localhost;Port=3306;Database=Course;Uid=root;Pwd=admin";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            return new MyContext (optionsBuilder.Options);
+        }
+    }
+}
